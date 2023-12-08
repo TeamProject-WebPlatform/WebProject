7482249cfc654198faf35dd4b7be624b7e798728
Doyun9710 <doyun1010k@gmail.com>
2023-12-08 10:53:03 +0900

    markdown viewer

--- src/main/java/platform/game/controller/MainController.java
+++ src/main/java/platform/game/controller/MainController.java
@@ -1,13 +1,22 @@
 package platform.game.controller;
 
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
+
+import platform.game.action.MypageAction;
 
 @Controller
 @ComponentScan(basePackages = {"platform.game.action"})
 public class MainController {
+
+    private MypageAction mypageAction = new MypageAction();
 
     @GetMapping("/")
     public ModelAndView main(){
@@ -18,4 +27,24 @@ public class MainController {
     public ModelAndView list(){
         return new ModelAndView("list");
     }
+    
+    @GetMapping("/mypage/{userid}")
+    public ModelAndView mypage(@PathVariable("userid") String userid, Model model){
+        String markdownValueFormLocal = null;
+
+        try {
+            markdownValueFormLocal = mypageAction.getMarkdownValueFormLocal( userid );
+        } catch (Exception e) {
+            System.out.println( "[에러] MainController : " + e.getMessage() );
+        }
+
+        Parser parser = Parser.builder().build();
+        Node document = parser.parse(markdownValueFormLocal);
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+
+        model.addAttribute("contents", renderer.render(document));
+
+        return new ModelAndView("mypage");
+    }
+    
 }
