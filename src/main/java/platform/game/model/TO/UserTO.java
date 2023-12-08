1910cfdcb4974d9f56069ff451c7f7cdf408dc7b
HappyBird6 <wsbg@naver.com>
2023-12-08 15:56:54 +0900

    userTO, userRankingInfoTO 수정

--- src/main/java/platform/game/model/TO/UserTO.java
+++ src/main/java/platform/game/model/TO/UserTO.java
@@ -7,12 +7,12 @@ public class UserTO {
     private String id;        // id, varchar(20) NOT NULL
     private String password;  // password, varchar(12) NOT NULL
     private String nickname;  // nickname, varchar(10) NOT NULL
-    private String u_mail;      // u_mail, varchar(50) NOT NULL
-    private String u_date;      // u_date, datetime NOT NULL
-    private int u_point;    // u_point, int NOT NULL
-    private int u_exp;      // u_exp, int NOT NULL
-    private int u_level;    // u_level, int NOT NULL
-    private int u_oauth;        // u_oauth, int NOT NULL
+    private String mail;      // mail, varchar(50) NOT NULL
+    private String date;      // date, datetime NOT NULL
+    private int point;    // point, int NOT NULL
+    private int exp;      // exp, int NOT NULL
+    private int level;    // level, int NOT NULL
+    private int oauth;        // oauth, int NOT NULL
     // 0 : 우리 웹, 1 : 카카오, 2 : 스팀 ...
 }
 
@@ -21,10 +21,10 @@ public class UserTO {
 // 	`id`	    varchar(20)	NOT NULL,
 // 	`password`	varchar(12)	NOT NULL,
 // 	`nickname`	varchar(10)	NOT NULL,
-// 	`u_mail`	varchar(50)	NOT NULL,
-// 	`u_date`	datetime	NOT NULL,
-// 	`u_point`	int	        NOT NULL,
-// 	`u_exp`	    int	        NOT NULL,
-// 	`u_level`	int	        NOT NULL,
-// 	`u_oauth`	int     	NOT NULL
+// 	`mail`	    varchar(50)	NOT NULL,
+// 	`date`	    datetime	NOT NULL,
+// 	`point`	    int	        NOT NULL,
+// 	`exp`	    int	        NOT NULL,
+// 	`level`	    int	        NOT NULL,
+// 	`oauth`	    int     	NOT NULL
 // );
