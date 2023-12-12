a7702cec2c58a5ed4f1852533128a3b0fef9961b
Doyun9710 <doyun1010k@gmail.com>
2023-12-12 16:20:31 +0900

    도윤) Mypage PostMapping Security Exception 해결

--- src/main/java/platform/game/security/SecurityConfig.java
+++ src/main/java/platform/game/security/SecurityConfig.java
@@ -4,13 +4,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable);
+
         return http.build();
     }
 }
