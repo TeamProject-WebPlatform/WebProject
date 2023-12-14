ce4c227fd55e727f537752760ed3f207f8877222
HappyBird6 <wsbg@naver.com>
2023-12-14 15:27:31 +0900

    123

--- src/main/java/platform/game/security/SecurityConfig.java
+++ src/main/java/platform/game/security/SecurityConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf(AbstractHttpConfigurer::disable);
 
