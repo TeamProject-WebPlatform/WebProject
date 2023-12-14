abd99f220ab53265678fc41735bd1985faebe5c3
yoonjusoung <redroon154@gmail.com>
2023-12-14 17:27:03 +0900

    123

---
+++ src/main/java/platform/game/action/MailAction.java
@@ -0,0 +1,11 @@
+package platform.game.action;
+
+public class MailAction implements BoardAction {
+
+    @Override
+    public void excute() {
+        System.out.println( "MypageAction 호출" );
+    }
+
+    
+}
