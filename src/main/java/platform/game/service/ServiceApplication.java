8a539f9d554c4875370248e002c82452fa5da2b7
chonggho <dudgh34544@gmail.com>
2023-12-14 09:41:44 +0900

    passwordEncoder 수정

--- src/main/java/platform/game/service/ServiceApplication.java
+++ src/main/java/platform/game/service/ServiceApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-import platform.game.security.CreateToken;
-
 @SpringBootApplication
 @ComponentScan(basePackages = { "platform.game.controller", "platform.game.env.config" })
 public class ServiceApplication {
@@ -13,6 +11,5 @@ public class ServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceApplication.class, args);
 	}
-
 
 }
