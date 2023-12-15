f69f47f2c29d00f54e8a45a5a960eb370bd41a4e
chonggho <dudgh34544@gmail.com>
2023-12-15 10:41:04 +0900

    board 쓰기,수정,삭제 테스트용 html페이지 추가

--- src/main/java/platform/game/controller/MainController.java
+++ src/main/java/platform/game/controller/MainController.java
@@ -1,4 +1,5 @@
 package platform.game.controller;
+
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -6,21 +7,22 @@ import org.springframework.web.servlet.ModelAndView;
 
 // Spring Security의 /login 페이지 안되게
 @Controller
-@ComponentScan(basePackages = {"platform.game.action","platform.game.env.config","platform.game.security"})
+@ComponentScan(basePackages = { "platform.game.action", "platform.game.env.config", "platform.game.security" })
 public class MainController {
 
     @GetMapping("/home")
-    public ModelAndView home(){
+    public ModelAndView home() {
         return new ModelAndView("index");
     }
+
     @GetMapping("/list")
-    public ModelAndView list(){
+    public ModelAndView list() {
         return new ModelAndView("list");
     }
-    @GetMapping("/show")
-    public ModelAndView show(){
+
+    @GetMapping("/shop")
+    public ModelAndView show() {
         return new ModelAndView("shop");
     }
 
-    
 }
