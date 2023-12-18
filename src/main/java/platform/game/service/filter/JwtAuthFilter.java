94ffa332938931da614617eb6107b9c8fc96c0a1
Happybird6 <wsbg@naver.com>
2023-12-18 11:50:00 +0900

    jwt filter 출력추가, 메인class @mapperscan 추가

--- src/main/java/platform/game/service/filter/JwtAuthFilter.java
+++ src/main/java/platform/game/service/filter/JwtAuthFilter.java
@@ -28,20 +28,20 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        System.out.println("필터 시작");
+        System.out.println("-----jwt 필터 시작-----");
         String authHeader = request.getHeader("Authorization"); 
-        System.out.println(authHeader);
+        System.out.println("header, Authorization : "+authHeader);
         String token = null; 
         String username = null; 
 
         if (authHeader != null && authHeader.startsWith("Bearer ")) { 
-            System.out.println("1 : 토큰 있음 추출");
+            System.out.println("jwt 필터 / 1 : 토큰 있음 추출");
             token = authHeader.substring(7); 
             username = jwtService.extractUsername(token); 
         } 
   
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) { 
-            System.out.println("2 : 유저 디테일 설정");
+            System.out.println("jwt 필터 / 2 : 유저 디테일 설정");
             UserDetails userDetails = userDetailsService.loadUserByUsername(username); 
             if (jwtService.validateToken(token, userDetails)) { 
                 UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities()); 
@@ -49,7 +49,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
                 SecurityContextHolder.getContext().setAuthentication(authToken); 
             } 
         } 
-        System.out.println("필터 끝");
+        System.out.println("-----jwt 필터   끝-----");
         filterChain.doFilter(request, response);   
         
     }
