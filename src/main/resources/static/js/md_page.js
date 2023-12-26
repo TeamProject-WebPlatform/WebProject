fb4710d3a16dd1db2724355569d82d5ed644a4b7
yoonjusoung <redroon154@gmail.com>
2023-12-26 17:09:37 +0900

    마이페이지 디자인 준완료

--- src/main/resources/static/js/md_page.js
+++ src/main/resources/static/js/md_page.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
     const editor = new toastui.Editor({
-        el: document.querySelector('#content'),      // 에디터를 적용할 요소 (컨테이너)
+        el: document.querySelector('#context'),      // 에디터를 적용할 요소 (컨테이너)
         height: '500px',                             // 에디터 영역의 높이 값 (OOOpx || auto)
         initialEditType: 'markdown',                 // 최초로 보여줄 에디터 타입 (markdown || wysiwyg)
         initialValue: '',                            // 내용의 초기 값으로, 반드시 마크다운 문자열 형태여야 함
@@ -38,4 +38,23 @@ document.addEventListener('DOMContentLoaded', function(){
             }
             /* end of hooks */
         });
+
+    document.getElementById('data').addEventListener('click', function () {
+        if (confirm("정보를 수정하시겠습니까?") == true){    //확인
+            document.getElementById("qrcode").style.display = "none";
+            document.getElementById("content").style.display = "block";
+        }else{   //취소
+            return false;
+        }
+    });
+    document.getElementById('data_check').addEventListener('click', function () {
+        if (confirm("정보를 수정하시겠습니까?") == true){    //확인
+            document.getElementById("qrcode").style.display = "block";
+            document.getElementById("content").style.display = "none";
+            location.reload();
+        }else{   //취소
+            return false;
+        }
+    });
+    
 });
