d8ea37364b399cffaf57bdf668cabfead874247a
yoonjusoung <redroon154@gmail.com>
2024-01-08 18:54:12 +0900

    아이템샵 스크립트 ㅅㅂ것

--- src/main/resources/static/js/shop.js
+++ src/main/resources/static/js/shop.js
@@ -1,4 +1,18 @@
-document.addEventListener('DOMContentLoaded', function(){
-    // 올리스트
-    
+document.addEventListener('DOMContentLoaded', function () {
+    var categorySelect = document.getElementById('categorySelect');
+    var gameCards = document.querySelectorAll('.game-card');
+
+    categorySelect.addEventListener('change', function () {
+        var selectedCategory = categorySelect.value;
+
+        gameCards.forEach(function (card) {
+            var cardCategory = card.getAttribute('data-category');
+
+            if (selectedCategory === 'all' || selectedCategory === cardCategory) {
+                card.style.display = 'block';
+            } else {
+                card.style.display = 'none';
+            }
+        });
+    });
 });
