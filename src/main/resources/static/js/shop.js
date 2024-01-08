1862c9538d79d1bd7dcc6cb2aa0cd2d475540d20
윤주성 <redroon154@gmail.com>
2024-01-08 20:51:54 +0900

    123

--- src/main/resources/static/js/shop.js
+++ src/main/resources/static/js/shop.js
@@ -1,4 +1,18 @@
 document.addEventListener('DOMContentLoaded', function(){
-    // 올리스트
-    
+    var categorySelect = document.getElementById('categorySelect');
+    var gameCards = document.querySelectorAll('.game-card');
+
+    categorySelect.addEventListener('change', function () {
+        var selectedCategory = categorySelect.value;
+
+        gameCards.forEach(function (card) {
+            var cardCategory = card.getAttribute('data-category');
+
+            if (selectedCategory === 'all' || selectedCategory === cardCategory) {
+                card.style.display = 'block';
+            } else {
+                card.style.display = 'none';
+            }
+        });
+    });
 });
