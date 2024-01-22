8ba75cb0d3644ac9969764f0215aa655d5da2bff
chonggho <dudgh34544@gmail.com>
2024-01-23 03:23:35 +0900

    가입 후 로그인 시 스와이퍼 오류 수정(테스트 필요)

--- src/main/java/platform/game/service/action/SignAction.java
+++ src/main/java/platform/game/service/action/SignAction.java
@@ -111,10 +111,9 @@ public class SignAction {
                 boolean flag = memService.addUser(member);
                 boolean addRank = memService.addUserRanking(member);
                 boolean addProfile = memService.addUserProfile(member);
-                boolean addFavGame = memService.addUserFavGame(member);
                 System.out.println("signupAction > 멤버 객체 추가 성공");
                 // 성공하면 common code의 값 update
-                if (flag && addRank && addProfile && addFavGame) {
+                if (flag && addRank && addProfile) {
                     comCdRepo.updateRemark1ByCd(idCode, String.valueOf(lastid));
                     userSignup.setMemId(id);    
                 } else {
